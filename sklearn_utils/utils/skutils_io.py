f819c259440070fd5d5ccae6fff608acc58139f7
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2017-11-28 12:53:47 +0300

    to_json added

--- sklearn_utils/utils/skutils_io.py
+++ sklearn_utils/utils/skutils_io.py
@@ -3,24 +3,26 @@ class SkUtilsIO:
     IO class to read and write dataset to file.
     '''
 
-    def __init__(filename, X=None, y=None, gz=False):
+    def __init__(filename, gz=False):
         '''
         :filename: file name with path.
-        :X: dataset as list of dict.
-        :y: labels.
         '''
         self.filename = filename
-        self.X = X
-        self.y = y
 
     def from_csv():
         pass
 
-    def to_csv():
+    def to_csv(X, y):
         pass
 
     def from_json():
         pass
 
-    def to_json():
-        pass
+    def to_json(X, y):
+        '''
+        :X: dataset as list of dict.
+        :y: labels.
+        '''
+        with gzip.open('%s.gz' % self.filename, 'wt') if self.gz else open(
+                self.path, 'w') as file:
+            json.dump(list(zip(X, y)), file)
