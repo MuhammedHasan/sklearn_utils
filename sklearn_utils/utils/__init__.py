10182b9f43934c118e8d94e1107a43464dee53b9
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2017-11-28 12:25:15 +0300

    skutilsio added

--- sklearn_utils/utils/__init__.py
+++ sklearn_utils/utils/__init__.py
@@ -1,8 +1,10 @@
 from .data_utils import *
+from .skutils_io import *
 
 __all__ = [
     'filter_by_label',
     'average_by_label',
     'map_dict',
     'map_dict_list',
+    'SkUtilsIO',
 ]
---
+++ sklearn_utils/utils/skutils_io.py
@@ -0,0 +1,26 @@
+class SkUtilsIO:
+    '''
+    IO class to read and write dataset to file.
+    '''
+
+    def __init__(filename, X=None, y=None, gz=False):
+        '''
+        :filename: file name with path.
+        :X: dataset as list of dict.
+        :y: labels.
+        '''
+        self.filename = filename
+        self.X = X
+        self.y = y
+
+    def from_csv():
+        pass
+
+    def to_csv():
+        pass
+
+    def from_json():
+        pass
+
+    def to_json():
+        pass
